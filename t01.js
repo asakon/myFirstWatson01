9072f299ccd000241d63df68d0718bebe252468d
asakon <kondoasako@gmail.com>
2020-05-16 18:21:11 +0900

    init

---
+++ t01.js
@@ -0,0 +1,23 @@
+const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const languageTranslator = new LanguageTranslatorV3({
+  version: '2018-05-01',
+  authenticator: new IamAuthenticator({
+    apikey: '{apikey}',
+  }),
+  url: '{url}',
+});
+
+const translateParams = {
+  text: 'Hello',
+  modelId: 'en-es',
+};
+
+languageTranslator.translate(translateParams)
+  .then(translationResult => {
+    console.log(JSON.stringify(translationResult, null, 2));
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
